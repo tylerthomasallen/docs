1b5b4c4ec7a8e0632bf34c9b9e62fbf31b8bf71d
Ethan Neff <ethan.neff@eneff.com>
2017-06-05 15:46:29 -0700

    feat: added docsearch code

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"></script>
+<script type="text/javascript">
+docsearch({
+  apiKey: '<API_KEY>',
+  indexName: '<INDEX_NAME>',
+  inputSelector: '<YOUR_INPUT_DOM_SELECTOR>'
+});
+</script>
