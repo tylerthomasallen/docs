c2326d6f6c992e2f35b13871efc5dff8d6a177dd
Ethan Neff <ethan.neff@eneff.com>
2017-07-20 12:16:03 -0700

    fix: begun analytics
    tested mix panel and works

--- src/js/main.js
+++ src/js/main.js
@@ -70,4 +70,17 @@
     };
   })();
 
+  // analytics
+  var analytics = (function() {
+    // public
+    function track(event, category, action) {
+      var categoryClean = category || 'none';
+      var actionClean = action || 'none';
+      ga('send', event, categoryClean, actionClean);
+      mixpanel.track(event);
+    }
+    return {
+      track: track
+    };
+  })();
 })();
