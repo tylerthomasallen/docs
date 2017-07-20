83787c9cffab9100027fba3ad984145b7c1eb0eb
Ethan Neff <ethan.neff@eneff.com>
2017-07-20 12:15:11 -0700

    fix: added pub sub

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
 // no globals and run on load
 (function() {
   'use strict';
+  // pub sub
+  var events = (function() {
+    // properties
+    var events = {};
+    // methods
+    function publish(name, data) {
+      if (events[name]) {
+        events[name].forEach(function(func) {
+          func(data);
+        });
+      }
+    }
+    function subscribe(name, func) {
+      events[name] = events[name] || [];
+      events[name].push(func);
+    }
+    function unsubscribe(name, func) {
+      if (events[name]) {
+        for (var i = 0; i < events[name].length; i++) {
+          if( events[name][i] === func ) {
+            events[name].splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+    // public
+    return {
+      publish: publish,
+      subscribe: subscribe,
+      unsubscribe: unsubscribe
+    };
+  })();
+
 })();
