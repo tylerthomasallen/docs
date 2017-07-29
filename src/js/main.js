b917f2749e2950c09025da70acf04a5cfada4767
Ethan Neff <ethan.neff@eneff.com>
2017-07-28 17:04:30 -0700

    fix: console error

--- src/js/main.js
+++ src/js/main.js
@@ -173,7 +173,7 @@
     }
     function _getImproveUrl() {
       var trim = window.location.pathname.replace(/^\/|\/$/g, '').split('/');
-      trim = trim[0] === 'docs' ? : trim.slice(1, trim.length);
+      trim = trim[0] === 'docs' ? trim.slice(1, trim.length) : trim;
       var path = (trim === '') ? '/index' : trim.join('/');
       return 'https://github.com/branchmetrics/docs/edit/master/src/' + path + '.md';
     }
