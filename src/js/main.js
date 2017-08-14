c8bac2d07b6b8d0bd3dee010ecc244fd37ed413c
Ethan Neff <ethan.neff@eneff.com>
2017-08-14 14:25:17 -0700

    feat: added click to expand images into modal

--- src/js/main.js
+++ src/js/main.js
@@ -66,6 +66,44 @@
     };
   })();
 
+  // images
+  var images = (function() {
+    var image = document.getElementById('modal-image-img');
+    var link = document.getElementById('modal-image-link')
+
+    // wrap all <img /> with <a href="image-[src]"><img /></a> to trigger expand modal
+    function init() {
+      var images = document.getElementsByTagName('img');
+      for (var i = 0; i < images.length; i++) {
+        var image = images[i];
+        if (image.parentElement.tagName !== 'A') {
+          var wrapper = document.createElement('a');
+          var src = image.getAttribute('src');
+          var hash = '#image-' + src;
+          wrapper.setAttribute('href', hash);
+          image.parentNode.insertBefore(wrapper, image);
+          wrapper.appendChild(image);
+          _route(hash, src);
+        }
+      }
+    }
+
+    function _route(hash, src) {
+      var route = 'route-' + hash;
+      if (events.items.hasOwnProperty(route)) return;
+      router.add(route, function() {
+        analytics.track('viewed modal image ' + src);
+        modals.toggle('modal-image', 'dialog', null, src);
+        link.setAttribute('href', src);
+        image.setAttribute('src', src);
+      });
+    }
+
+    return {
+      init: init
+    };
+  })();
+
   // modals
   var modals = (function() {
     // properties
@@ -499,6 +537,7 @@
     analytics.track('viewed page ' + window.location.href);
     buttons.init();
     codeTabs.init();
+    images.init();
     modals.route();
     router.init();
   }
