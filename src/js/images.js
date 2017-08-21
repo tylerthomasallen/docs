641e92457799ffe584159d4e8d967a2c77dc8640
Ethan Neff <ethan.neff@eneff.com>
2017-08-21 09:39:47 -0700

    feat: images expand now with query string

--- src/js/images.js
+++ src/js/images.js
@@ -1,35 +1,58 @@
 // images
 'use strict';
 var images = (function() {
+  // cache
   var image = document.getElementById('modal-image-img');
-  var link = document.getElementById('modal-image-link')
+  var link = document.getElementById('modal-image-link');
+  var hash = '#dialog-image';
 
   // wrap all <img /> with <a href="image-[src]"><img /></a> to trigger expand modal
   function init() {
+    console.log('init')
+    var images = _scrapeImages();
+    _wrapImages(images);
+    _addRoute();
+  }
+
+  function _scrapeImages() {
     var images = document.getElementsByTagName('img');
+    var output = []
     for (var i = 0; i < images.length; i++) {
       var image = images[i];
       if (image.parentElement.tagName !== 'A') {
-        var wrapper = document.createElement('a');
-        var src = image.getAttribute('src');
-        var hash = '#modal-image=' + src;
-        wrapper.setAttribute('href', hash);
-        image.parentNode.insertBefore(wrapper, image);
-        wrapper.appendChild(image);
-        _route(hash, src);
+        output.push(image);
+
       }
+    }
+    return output;
+  }
+
+  function _wrapImages(images) {
+    for (var i = 0; i < images.length; i++) {
+      var image = images[i]
+      var wrapper = document.createElement('a');
+      var src = image.getAttribute('src');
+      var href = hash + "?src=" + src;
+      wrapper.setAttribute('href', href);
+      image.parentNode.insertBefore(wrapper, image);
+      wrapper.appendChild(image);
     }
   }
 
-  function _route(hash, src) {
-    var route = 'route-' + hash;
-    if (events.items.hasOwnProperty(route)) return;
-    router.add(route, function() {
-      analytics.track('viewed modal image ' + src);
-      modals.toggle('modal-image', 'dialog', null, src);
-      link.setAttribute('href', src);
-      image.setAttribute('src', src);
+  function _addRoute() {
+    console.log('addroute')
+    router.add(hash, function() {
+      _displayRoute();
     });
+  }
+
+  function _displayRoute() {
+    var src = utils.readQueryString('src');
+    if (!src) return;
+    analytics.track('viewed modal image ' + src);
+    modals.toggle('modal-image', 'dialog');
+    link.setAttribute('href', src);
+    image.setAttribute('src', src);
   }
 
   return {
--- src/js/router.js
+++ src/js/router.js
@@ -3,17 +3,23 @@
 var router = (function() {
   // listener
   window.addEventListener('hashchange', function(event) {
-    _route(event.target.location.hash);
+    var hash = _cleanHash(event.target.location.hash);
+    _route(hash);
   }, false);
 
   // private
   function _route(hash) {
     events.publish(hash);
   }
+  function _cleanHash(hash) {
+    // remove query strings
+    return hash.split("?")[0];
+  }
 
   // public
   function init() {
-    _route(window.location.hash);
+    var hash = _cleanHash(window.location.hash);
+    _route(hash);
   }
   function add(hash, func) {
     events.subscribe(hash, func);
