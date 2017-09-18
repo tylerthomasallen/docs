081ed041f30969740172384ce552e9b9a47165cb
Ethan Neff <ethan.neff@eneff.com>
2017-09-18 14:42:48 -0700

    fix: corrected pageload js

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -3,10 +3,12 @@
 var analytics = (function() {
   // public
   function track(event, category, action) {
-    var categoryClean = category || 'none';
-    var actionClean = action || 'none';
-    ga('send', event, categoryClean, actionClean);
-    mixpanel.track(event);
+    utils.waitForPageLoad(function() {
+      var categoryClean = category || 'none';
+      var actionClean = action || 'none';
+      ga('send', event, categoryClean, actionClean);
+      mixpanel.track(event);
+    });
   }
   return {
     track: track
